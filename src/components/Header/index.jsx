762b81cb06836a72ae666cd9d865ddc11a105a5b
marc.nzokou.git.preprod <marc.nzokou.git.preprod@gmail.com>
2018-02-08 00:19:49 +0100

    update headerbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,19 +3,19 @@
  */
 import React from 'react';
 import ReactTooltip from 'react-tooltip';
-import { MdAccountCircle, MdChevronLeft, MdChevronRight, MdExpandMore } from 'react-icons/lib/md';
+import {  MdChevronLeft, MdChevronRight, MdExpandMore } from 'react-icons/lib/md';
 import './styles.scss';
 import Calendar from './Calendar';
 
 const Header = () => (
     <div className='header'>
         <a><MdChevronLeft /></a>
-        <a data-tip data-for='calendar' data-event='click focus'>Février 2018</a>
+        <a data-tip data-for='calendar' data-event='click focus'><span className="bold">Février</span> 2018</a>
         <ReactTooltip id='calendar' type='light'>
             <Calendar />
         </ReactTooltip>
         <a><MdChevronRight /></a>
-        <a className="f-right"><MdAccountCircle /><MdExpandMore /></a>
+        <a className="f-right">Marc N<MdExpandMore /></a>
     </div>
 );
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,7 +10,7 @@ import './styles.scss';
 const Navbar = () => (
     <nav>
         <div className='header'>
-            <span>Tontine</span><span className="plus">Plus</span>
+            <span>Tontine</span><span className="plus"> Plus</span>
         </div>
         <ul>
             <li>
