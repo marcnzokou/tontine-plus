dd29fba8d6fcc7c047233c30a2b766c3657b6a76
marc.nzokou.git.preprod <marc.nzokou.git.preprod@gmail.com>
2018-02-26 08:05:41 +0100

    (Reducer: Members) update state

--- src/containers/Home/Memberships/index.jsx
+++ src/containers/Home/Memberships/index.jsx
@@ -8,26 +8,26 @@ import './styles.scss';
 import MembershipItem from './MembershipItem';
 
 
-const Memberships = ({ listMembers }) => (
+const Memberships = ({ members }) => (
     <div className="membership">
-        <div className="title">Membres <span>(0)</span></div>
+        <div className="title">Membres <span>({members.count})</span></div>
         <div className="formControl search">
             <input type="text" placeholder="Rechercher un membre..." />
         </div>
         <div className="list">
             <ul>
-                {listMembers.map(function(member, index){
+                {members.datas.map(function(member, index){
                     return <li key={index}><MembershipItem item={member} /></li>;
                 })}
             </ul>
         </div>
-        
+
     </div>
 );
 
 const mapStateToProps = state => {
     return {
-       listMembers: state.members
+        members: state.members
     };
 };
 
--- src/reducers/members.js
+++ src/reducers/members.js
@@ -2,18 +2,26 @@ import {
     ADD_TO_MEMBER
 } from '../constants/ActionTypes';
 
-const members = (state = [], action) => {
-    switch (action.type) {
+const initialState = {
+    count: 0,
+    datas: []
+};
 
-      case ADD_TO_MEMBER:
-        return [
-            ...state,
-            action.payload
-        ];
-      
-      default:
-        return state;
+const members = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_MEMBER:
+            return {
+                ...state,
+                count: state.count + 1,
+                datas: [
+                    ...state.datas,
+                    action.payload
+                ]
+            };
+        default:
+            return state;
     }
 };
+
 
 export default members;
